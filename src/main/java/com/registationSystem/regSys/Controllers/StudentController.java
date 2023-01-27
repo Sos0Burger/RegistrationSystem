ee2313bfbac4abb8e3b234027308bb12abbe0723
Ixor <stulnikovan1337@gmail.com>
2023-01-27 21:44:05 +0300

    springdoc swagger-ui.html fix

--- src/main/java/com/registationSystem/regSys/Controllers/StudentController.java
+++ src/main/java/com/registationSystem/regSys/Controllers/StudentController.java
@@ -2,8 +2,6 @@ package com.registationSystem.regSys.Controllers;
 
 import com.registationSystem.regSys.Student;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
